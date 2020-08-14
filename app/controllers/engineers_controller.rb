3aaadfb7f08eab3256e32b8ebe28c8ffc5c2dbf3
dylan-williamson <dylwilliamson710@gmail.com>
2020-08-14 10:24:44 -0400

    adds views templates

--- app/controllers/engineers_controller.rb
+++ app/controllers/engineers_controller.rb
@@ -1,2 +1,12 @@
 class EngineersController < ApplicationController
+
+    def index
+
+    end
+
+    def show
+
+    end
+
+    
 end
--- app/controllers/services_controller.rb
+++ app/controllers/services_controller.rb
@@ -1,2 +1,12 @@
 class ServicesController < ApplicationController
+
+    def index
+
+    end
+
+    def show
+
+    end
+
+    
 end
