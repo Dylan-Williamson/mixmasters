36d6e5dc0b59211366fd7604e0a240b8aba9efb9
dylan-williamson <dylwilliamson710@gmail.com>
2020-08-17 09:23:24 -0400

    updates routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,15 +1,7 @@
 Rails.application.routes.draw do
-  resources :orders, except: [:show, ]
-  resources :clients, except: [:index]
+  resources :orders, except: [:new, :index, :edit]
+  resources :clients, except: [:new, :index]
   resources :services
   resources :engineers
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
-
-new
-create
-index
-show
-edit
-update
-destroy
