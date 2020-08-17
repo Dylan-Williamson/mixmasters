1bdf26b606118270b97fb161e322474fee401e17
dylan-williamson <dylwilliamson710@gmail.com>
2020-08-17 16:30:39 -0400

    adds sessions controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
+  get 'sessions/new'
+  get 'sessions/create'
+  get 'sessions/destroy'
   resources :orders, only: [:create, :update, :index, :show]
   resources :clients, except: [:new, :index]
   resources :services
