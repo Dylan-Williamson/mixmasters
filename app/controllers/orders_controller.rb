eb9fa622049464e8961d2cf8df2b4f8cb924cb1b
dylan-williamson <dylwilliamson710@gmail.com>
2020-08-17 10:34:23 -0400

    updates orders controller actions

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,16 +1,26 @@
 class OrdersController < ApplicationController
-    def create
+    before_action :find_order, only: [:show, :update]
 
+    def create
+        @order = Order.create(order_params)
     end
 
     def update
+        @order.update(order_params)
+        redirect_to @order
     end
 
     def show
-
+        render 'show'
     end
 
-    def destroy
+    private
 
+    def order_params
+        params.require(:order).permit(:username)
+    end
+
+    def find_order
+        @order = Order.find(params[:id])
     end
 end
