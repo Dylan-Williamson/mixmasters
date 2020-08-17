6bb9f6b6d031c35c6846f9e640a713542bc302b7
dylan-williamson <dylwilliamson710@gmail.com>
2020-08-17 09:06:17 -0400

    --- Auto-Git Commit ---

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,15 @@
 Rails.application.routes.draw do
-  resources :orders
-  resources :clients
+  resources :orders, except: [:show, ]
+  resources :clients, except: [:index]
   resources :services
   resources :engineers
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
+
+new
+create
+index
+show
+edit
+update
+destroy
