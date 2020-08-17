235afa4724e6d7b83e16984a17266a7234f612ae
dylan-williamson <dylwilliamson710@gmail.com>
2020-08-17 10:28:20 -0400

    updates client controller actions

--- app/controllers/clients_controller.rb
+++ app/controllers/clients_controller.rb
@@ -1,20 +1,35 @@
 class ClientsController < ApplicationController
-    def create
+    before_action :find_client, only: [:edit, :update, :show, :destroy]
 
+    def create
+        @client = Client.create(client_params)
     end
 
     def edit
-
+        render 'edit'
     end
 
     def update
+        @client.update(client_params)
+        redirect_to @client
     end
 
     def show
-
+        render 'show'
     end
 
     def destroy
+        @client.destroy
+        redirect_to services_path
+    end
 
+    private
+
+    def client_params
+        params.require(:client).permit(:username)
+    end
+
+    def find_client
+        @client = Client.find(params[:id])
     end
 end
