d37021880faa7f751d211b7dbafc4afc1e0869fc
dylan-williamson <dylwilliamson710@gmail.com>
2020-08-17 08:39:03 -0400

    adds seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,7 @@
 Engineer.create(name: "Dylan Williamson", username: "dylwill", experience: "Moderate", location: "Saratoga Springs, NY", bio: "New York based Mixing and Mastering Engineer.")
 
 Service.create(title: "Multi-Track Mixing", description: "Professional quality multi-track mixing.", price: 250, requirements: "Pro Tools Session ZIPPED", engineer_id: 1)
+
+Client.create(username: "joeexotic")
+
+Order.create(engineer_id: 1, service_id: 1, client_id: 1)
