ffb8aad3cac6692df1bcc729186ab5945ae6c814
dylan-williamson <dylwilliamson710@gmail.com>
2020-08-19 14:20:16 -0400

    fixes order validation

--- app/models/engineer.rb
+++ app/models/engineer.rb
@@ -1,4 +1,6 @@
 class Engineer < ApplicationRecord
+    validates :username, :name, :location, :bio, :experience, presence: true
+
     has_many :services
     has_many :orders
     has_many :reviews, through: :orders
--- app/models/order.rb
+++ app/models/order.rb
@@ -1,5 +1,5 @@
 class Order < ApplicationRecord
-    validates :client_id, :engineer_id, :service_id
+    validates :client_id, :engineer_id, :service_id, presence: true
 
     belongs_to :engineer
     belongs_to :client
