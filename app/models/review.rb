b08ea2079fd16b8a9c27d6d489f279852f4c72ed
dylan-williamson <dylwilliamson710@gmail.com>
2020-08-19 14:11:26 -0400

    adds presence validations to review model

--- app/models/review.rb
+++ app/models/review.rb
@@ -1,4 +1,6 @@
 class Review < ApplicationRecord
+    validates :content, :rating, :engineer_id, :client_id, :service_id, :order_id, presence: true
+
     belongs_to :service
     belongs_to :order
     belongs_to :engineer
