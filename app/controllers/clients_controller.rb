8dc000732bb6f29b630d1a0e1df69b877a5a1aec
dylan-williamson <dylwilliamson710@gmail.com>
2020-08-22 15:21:40 -0400

    updates client params

--- app/controllers/clients_controller.rb
+++ app/controllers/clients_controller.rb
@@ -26,7 +26,7 @@ class ClientsController < ApplicationController
     private
 
     def client_params
-        params.require(:client).permit(:username)
+        params.require(:client).permit(:username, :password, :password_confirmation)
     end
 
     def find_client
--- app/models/engineer.rb
+++ app/models/engineer.rb
@@ -1,8 +1,8 @@
 class Engineer < ApplicationRecord
-    validates :username, :name, :location, :bio, :experience, presence: true
-
     has_many :services
     has_many :orders
     has_many :reviews, through: :orders
     has_many :clients, through: :orders
+    validates :username, :name, :location, :bio, :experience, presence: true
+    has_secure_password
 end
