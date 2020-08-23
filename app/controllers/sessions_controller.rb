65634da7b93c1864308c2a1a13d524f06cfb7761
dylan-williamson <dylwilliamson710@gmail.com>
2020-08-23 13:58:06 -0400

    updates create method

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,7 +3,14 @@ class SessionsController < ApplicationController
   end
 
   def create
+    engineer = Engineer.find_by(username: params[:username])
+    if engineer && engineer.authenticate(params[:password])
 
+      redirect_to services_path
+    else
+      flash[:errors] = "Invalid Credentials"
+      redirect_to login_path
+    end
   end
 
   def destroy
