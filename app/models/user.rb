dc6a76023035dbd95f55e46933fab364527bce57
dylan-williamson <dylwilliamson710@gmail.com>
2020-08-23 16:57:06 -0400

    --- Auto-Git Commit ---

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,2 @@
-class Engineer < ApplicationRecord
-    validates :username, :name, :location, :bio, presence: true
-    has_many :services
-    has_many :orders
-    has_many :reviews, through: :orders
-    has_secure_password
+class User < ApplicationRecord
 end
--- db/migrate/20200823205430_create_users.rb
+++ db/migrate/20200823205430_create_users.rb
@@ -3,9 +3,10 @@ class CreateUsers < ActiveRecord::Migration[6.0]
     create_table :users do |t|
       t.string :username
       t.string :name
+      t.string :password_digest
       t.string :location
-      t.integer :rating
       t.text :bio
+      t.integer :rating
 
       t.timestamps
     end
