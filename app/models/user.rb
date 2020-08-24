2d33e4198bd7501fc25e612554ca1f8c3486e865
dylan-williamson <dylwilliamson710@gmail.com>
2020-08-23 20:39:30 -0400

    updates validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,6 @@
 class User < ApplicationRecord
     validates :username, :name, :location, :bio, presence: true
+    validates :username, uniqueness: true
     has_many :services
     has_many :orders
     has_many :reviews, through: :orders
