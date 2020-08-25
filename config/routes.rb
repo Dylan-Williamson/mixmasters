19f83e0054924bc5de4e9f1cd176b058282edeef
dylan-williamson <dylwilliamson710@gmail.com>
2020-08-25 13:52:27 -0400

    fixes delete functionality of services

--- config/routes.rb
+++ config/routes.rb
@@ -7,7 +7,7 @@ Rails.application.routes.draw do
     resources :reviews, only: [:new, :create, :index]
   end
   resources :users, only: [:new, :create, :edit, :update, :show]
-  resources :services
+  resources :services 
   # resources :reviews, only: [:create]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
