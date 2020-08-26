56c1e9322e1d6e133992b125e6f743a21a09a41d
dylan-williamson <dylwilliamson710@gmail.com>
2020-08-26 15:25:08 -0400

    adds signup route

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,13 @@
 Rails.application.routes.draw do
   get '/login', to: "sessions#new", as: "/login"
+  get '/signup', to: "users#new", as: "/signup"
   post '/sessions', to: "sessions#create", as: "sessions"
   delete '/sessions', to: "sessions#destroy"
   get 'auth/google_oauth2/callback', to: "sessions#create_with_google_omniauth"
   resources :orders, only: [:create, :update, :index, :show] do
     resources :reviews, only: [:new, :create, :index]
   end
-  resources :users, only: [:new, :create, :edit, :update, :show]
+  resources :users, only: [:create, :edit, :update, :show]
   resources :services 
   # resources :reviews, only: [:create]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
